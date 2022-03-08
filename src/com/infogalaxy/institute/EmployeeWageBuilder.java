1db9e78c4a3a4d934564eed87b9725d619f21d16
mayurmore14 <mayurmore8505@gmail.com>
2022-03-08 10:37:05 +0530

    MODIFIED: Solving Using Switch Case

--- src/com/infogalaxy/institute/EmployeeWageBuilder.java
+++ src/com/infogalaxy/institute/EmployeeWageBuilder.java
@@ -8,30 +8,34 @@ public class EmployeeWageBuilder {
         System.out.println("Welcome to Employee Wage Computation program Developed by Mayur");
         //UC-2 Calculate Daily Employee Wage
         final int WAGE_PER_HR = 20;
+        //UC-3 Checking Employee is Part Time or Full time
         final int FULL_TIME_HOUR = 8;
         final int PART_TIME_HOUR = 4;
         int total_wage = 0;
         //UC-1 employee Attendance
-        Random random=new Random();
-        int empAttendance =random.nextInt()%2;
+        Random random = new Random();
+        int empAttendance = random.nextInt()%2;
         System.out.println(empAttendance);
         if(empAttendance == 0) {
             System.out.println("Employee is Absent");
         } else {
             System.out.println("Employee is Present");
-            //UC-3 Add Part Time Employee and Wage
-            Random random1 = new Random();
+
             int time = random.nextInt()%2;
-            if(time == 0) {
-                System.out.println("Employee is Part Time");
-                total_wage=WAGE_PER_HR * PART_TIME_HOUR;
-            } else {
-                System.out.println("Employee is Full Time");
-                total_wage = WAGE_PER_HR * FULL_TIME_HOUR;
+            //UC-4 Solving Using Switch Case Statement
+            switch(time) {
+                case 0:
+                    System.out.println("Employee is Part Time");
+                    total_wage = WAGE_PER_HR * PART_TIME_HOUR;
+                    break;
+                case 1:
+                case -1:
+                    System.out.println("Employee is Full Time");
+                    total_wage = WAGE_PER_HR * FULL_TIME_HOUR;
+                    break;
             }
         }
         System.out.println("Total Wage = "+total_wage);
-
 
     }
 }
